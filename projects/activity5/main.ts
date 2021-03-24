13f5756f2ff41203813345f228f2feef8987e9f4
shakao <shakao@microsoft.com>
2021-03-24 09:16:48 -0700

    Add activity 3 (quail) template

---
+++ projects/activity5/main.ts
@@ -0,0 +1,67 @@
+namespace SpriteKind {
+    //% isKind
+    export const Quail = SpriteKind.create()
+    //% isKind
+    export const Egg = SpriteKind.create()
+}
+sprites.onDestroyed(SpriteKind.Egg, function (sprite) {
+    quail = sprites.create(img`
+        f f . . . . . . . . . . 
+        . . f . . . . . . . . . 
+        . c c c c . . . . . . . 
+        . c 1 c c b . . . . . . 
+        f c c c b d b . . . . . 
+        . . b b b d d b . . . . 
+        . . b b b d b d b . . . 
+        . . b b b b b d d b b b 
+        . . b b b b b b b b b b 
+        . . . . b b b b b . . . 
+        . . . . . 4 . 4 . . . . 
+        . . . . . 4 . . . . . . 
+        `, SpriteKind.Quail)
+    quail.setPosition(sprite.x, sprite.y)
+    quail.setVelocity(randint(80, 120), randint(80, 120))
+    quail.setBounceOnWall(true)
+    quail.z = 10
+})
+
+let quail_egg: Sprite = null
+let quail: Sprite = null
+for (let index = 0; index < randint(3, 5); index++) {
+    quail = sprites.create(img`
+        f f . . . . . . . . . . 
+        . . f . . . . . . . . . 
+        . c c c c . . . . . . . 
+        . c 1 c c b . . . . . . 
+        f c c c b d b . . . . . 
+        . . b b b d d b . . . . 
+        . . b b b d b d b . . . 
+        . . b b b b b d d b b b 
+        . . b b b b b b b b b b 
+        . . . . b b b b b . . . 
+        . . . . . 4 . 4 . . . . 
+        . . . . . 4 . . . . . . 
+        `, SpriteKind.Quail)
+    quail.setVelocity(randint(-100, 100), randint(-100, 100))
+    quail.setBounceOnWall(true)
+    quail.z = 10
+    tiles.placeOnRandomTile(quail, sprites.castle.tilePath5)
+}
+game.onUpdateInterval(2000, function () {
+    if (sprites.allOfKind(SpriteKind.Quail).length < 80) {
+        for (let value of sprites.allOfKind(SpriteKind.Quail)) {
+            quail_egg = sprites.create(img`
+                . . . c c . . . 
+                . . c 1 1 c . . 
+                . c b 1 1 b c . 
+                c b 1 1 1 b b c 
+                c 1 1 1 1 1 1 c 
+                c 1 1 b 1 1 1 c 
+                . c 1 1 1 b c . 
+                . . c c c c . . 
+                `, SpriteKind.Egg)
+            quail_egg.setPosition(value.x, value.y)
+            quail_egg.lifespan = randint(2000, 3000)
+        }
+    }
+})
